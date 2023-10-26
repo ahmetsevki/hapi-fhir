ed7a4ec366d83eea9a6cb568d95b809eb1b47626
Ahmet Nalcacioglu <ahmet@nalch.com>
2023-10-26 18:11:40 -0400

    fix clob issue in oracle

--- hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceHistoryTable.java
+++ hapi-fhir-jpaserver-model/src/main/java/ca/uhn/fhir/jpa/model/entity/ResourceHistoryTable.java
@@ -25,9 +25,7 @@ import ca.uhn.fhir.rest.api.Constants;
 import jakarta.persistence.*;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.OptimisticLock;
-import org.hibernate.type.SqlTypes;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -91,9 +89,9 @@ public class ResourceHistoryTable extends BaseHasResource implements Serializabl
 	@OptimisticLock(excluded = true)
 	private byte[] myResource;
 
-	@Column(name = "RES_TEXT_VC", length = RES_TEXT_VC_MAX_LENGTH, nullable = true)
-	@JdbcTypeCode(SqlTypes.LONG32VARCHAR)
-	@OptimisticLock(excluded = true)
+	@Column(name = "RES_TEXT_VC", nullable = true)
+	@Lob
+	@OptimisticLock(excluded = true) //
 	private String myResourceTextVc;
 
 	@Column(name = "RES_ENCODING", nullable = false, length = ENCODING_COL_LENGTH)
